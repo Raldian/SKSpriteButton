81f53d2fe58864dfe4b05a69e486b943ecaf0358
Casper Herse <casp095a@gmail.com>
2014-09-20 04:43:50 +0200

    Recoded the scripts

--- SKSpriteButton.m
+++ SKSpriteButton.m
@@ -1,37 +1,106 @@
 #import "SKSpriteButton.h"
+#import "Constants.h"
+
+typedef void (^SKSpriteButtonNodeSelectedBlock)(SKSpriteButton *buttonNode);
+
+@interface SKSpriteButton ()
+    @property (nonatomic, copy) SKSpriteButtonNodeSelectedBlock selectedBlock;
+
+    @property (nonatomic, assign) BOOL isSelected;
+@end
 
 @implementation SKSpriteButton
 
--(SKSpriteButton *) initWithType:(NSUInteger)type label:(NSString *)txt{
-    self = [super initWithTexture:[SKTexture textureWithImageNamed:@"button"]];
++ (instancetype)buttonNodeWithType:(NSUInteger)type label:(NSString *)txt block:(void (^)(id))block{
+    return [[self alloc] initButtonNodeWithType:type label:txt block:block];
+}
+
+- (instancetype)initButtonNodeWithType:(NSUInteger)type label:(NSString *)txt block:(void (^)(id))block{
     
+    self = [super initWithTexture:[SKTexture textureWithImageNamed:@"button"]];
     if(self){
+        _selectedBlock = block;
+        self.userInteractionEnabled = YES;
+        
         self.centerRect = CGRectMake(20.0/40.0, 20.0/40.0, 5.0/40.0, 5.0/40.0);
         self.name = txt;
         
-        if(type == 1)
-            self.xScale = 768/40;
-        else if(type == 2)
-            self.xScale = ([txt length]*20)/40;
-        else
-            self.xScale = ([txt length]*20)/40;
+        switch (type) {
+            case 1:
+                self.xScale = [[UIScreen mainScreen] bounds].size.width/41;
+                self.yScale = (self.size.height+20)/(IS_IPHONE ? 65 : 40);
+                break;
+                
+            case 2:
+                self.xScale = ([[UIScreen mainScreen] bounds].size.width/42)/2;
+                self.yScale = (self.size.height+20)/(IS_IPHONE ? 65 : 40);
+                break;
+                
+            case 3:
+                self.xScale = [[UIScreen mainScreen] bounds].size.height/(IS_IPHONE ? 65 : 55);
+                self.yScale = (self.size.height)/(IS_IPHONE ? 65 : 20);
+                break;
+                
+            case 4:
+                self.xScale = ([[UIScreen mainScreen] bounds].size.width/42)/2;
+                self.yScale = (self.size.height+20)/(IS_IPHONE ? 65 : 40);
+                break;
+                
+            default:
+                self.xScale = ([txt length]*20)/(IS_IPHONE ? 65 : 40);
+                self.yScale = (self.size.height+20)/(IS_IPHONE ? 65 : 40);
+                break;
+        }
         
-        if(type == 2)
-            self.yScale = (self.size.height)/40;
-        else
-            self.yScale = (self.size.height+20)/40;
-        
-        SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        SKLabelNode *label = [SKLabelNode labelNodeWithFontNamed:@"CardCharacters"];
         label.text = txt;
         label.name = txt;
-        label.fontSize = 30;
-        label.yScale = label.fontSize/self.size.height;
-        label.xScale = label.fontSize/self.size.width;
-        label.position = CGPointMake(CGRectGetMidX(self.frame), self.size.height/2-label.fontSize);
+        
+        label.yScale = 30/self.size.height;
+        label.xScale = 30/self.size.width;
+        
+        label.fontSize = self.size.height - (IS_IPHONE ? 7.5 : 0);
+        
+        label.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMaxY(self.frame) - self.size.height/2 - label.frame.size.height/2);
+        
         [self addChild:label];
     }
     
     return self;
+}
+
+- (void)selected{
+    self.isSelected = YES;
+}
+
+- (void)unSelected{
+    self.isSelected = NO;
+}
+
+- (void)activate{
+    if(self.selectedBlock){
+        __weak SKSpriteButton *buttonNode = self;
+        self.selectedBlock(buttonNode);
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (self.hidden || self.parent.hidden) {
+        return;
+    }
+    
+    [self selected];
+}
+
+- (void)touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self unSelected];
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    if (self.isSelected) {
+        [self unSelected];
+        [self activate];
+    }
 }
 
 @end
