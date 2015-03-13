26a21643b5d4147e2b20909522bcfe6f1305049c
Casper Herse <casp095a@gmail.com>
2015-03-13 01:04:10 +0100

    Added +buttonNodeWithType:image:label:block; & -changeButtonLabel;

--- SKSpriteButton.h
+++ SKSpriteButton.h
@@ -1,9 +1,16 @@
-#import <SpriteKit/SpriteKit.h>
+@import SpriteKit;
 
 @interface SKSpriteButton : SKSpriteNode
 
 + (instancetype) buttonNodeWithType:(NSUInteger) type
                               label:(NSString *) txt
                               block:(void(^)(id buttonNode))block;
+
++ (instancetype) buttonNodeWithType:(NSUInteger)type
+                              image:(SKTexture *)texture
+                              label:(NSString *)txt
+                              block:(void (^)(id))block;
+
+- (void) changeButtonLabel:(NSString *) label;
 
 @end
